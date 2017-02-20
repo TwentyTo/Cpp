8516ab34689b5e80ec76cc919a3ba72449047c42
Alexey Samoylov <ASamoylov@SPB.luxoft.com>
2017-02-20 21:02:47 +0300

    Arrays

---
+++ arrays/jvectorTest.hpp
@@ -0,0 +1,19 @@
+#pragma once
+namespace arrays 
+{
+class JVectorTest {
+public:
+   void RunTests() const;
+   void TestSize() const;
+   void TestCapacity() const;
+   void TestIsEmpty() const;
+   void TestGetValueAt() const;
+   void TestPop() const;
+   void TestInsert() const;
+   void TestPrepend() const;
+   void TestDelete() const;
+   void TestRemove() const;
+   void TestFind() const;
+   void TestTypes() const;
+};
+}
